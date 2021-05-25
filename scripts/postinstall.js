6bb6165a86ab65a8c3021ef7648be1f49d55f2f8
swimauger <swimauger@gmail.com>
2021-05-24 21:40:17 -0500

    ✨ Create postinstall script

---
+++ scripts/postinstall.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = (TSC_BIN_PATH) => {
+  const INJECTION_PATH = path.resolve(__dirname, '../lib/injection');
+
+  const script = [
+    '#!/usr/bin/env node',
+    `require('${path.relative(path.dirname(TSC_BIN_PATH), INJECTION_PATH)}')`,
+    "require('../lib/tsc.js')"
+  ];
+
+  fs.writeFileSync(TSC_BIN_PATH, script.join('\n'));
+}
