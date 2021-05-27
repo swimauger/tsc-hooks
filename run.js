68e35729725b5d2e800c55863ef7070acc1bad86
swimauger <swimauger@gmail.com>
2021-05-27 15:27:11 -0500

    ⚗️ Checking if execution ever happens

--- run.js
+++ run.js
@@ -6,24 +6,11 @@ const PACKAGE_JSON = require(path.resolve(PROJECT_ROOT, 'package.json'));
 const script = require(path.resolve(__dirname, 'scripts', process.argv[2]));
 const isGlobalScript = !(PACKAGE_JSON.dependencies?.typescript || PACKAGE_JSON.devDependencies?.typescript);
 
-if (isGlobalScript && process.argv[2] === 'preinstall') {
-  const readline = require('readline');
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  rl.question('In order to use tsc-hooks typescript must be installed as a devDependency. To install, type y/N:', (answer) => {
-    if (!answer || answer.toLowerCase() === 'y') {
-      execSync('npm i typescript --save-dev --no-package-lock');
-      script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
-      rl.close();
-    } else {
-      throw `Must install typescript either as a dependency or devDependency of this project.
-Run: npm i typescript --save-dev # or npm i typescript`;
-    }
-  });
+console.log(process.argv[2]);
+if (isGlobalScript && process.argv[2] === 'postinstall') {
+  execSync('npm i typescript --save-dev --no-package-lock');
+  console.log('Downloaded TypeScript... I think?');
+  script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
 } else {
   script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
 }
