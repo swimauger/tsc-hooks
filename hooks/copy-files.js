afc95ff4c4441ed904e69e26e562f37b7a0411bf
swimauger <swimauger@gmail.com>
2021-06-09 09:53:15 -0500

    🐛 Fix missing parent directories issue

--- hooks/copy-files.js
+++ hooks/copy-files.js
@@ -11,9 +11,22 @@ module.exports = (tsconfig, tsconfigPath) => {
     const files = include?.reduce((files, file) => glob.sync(file, { ignore }), []) || [];
     for (const file of files) {
       if (file.endsWith('.js') || file.endsWith('.ts')) continue;
+
       const relative = file.replace(path.resolve(file, path.relative(file, tsconfigDir)), '').split('/').splice(2).join('/');
-      const fileContent = fs.readFileSync(file);
-      fs.writeFileSync(path.resolve(tsconfigDir, tsconfig.compilerOptions.outDir, relative), fileContent);
+      const target = path.resolve(tsconfigDir, tsconfig.compilerOptions.outDir, relative);
+
+      if (!fs.existsSync(path.dirname(target))) {
+        fs.mkdirSync(path.dirname(target), { recursive: true });
+      }
+
+      if (fs.lstatSync(file).isDirectory() && !fs.existsSync(target)) {
+        fs.mkdirSync(target, { recursive: true });
+      }
+
+      if (fs.lstatSync(file).isFile()) {
+        const fileContent = fs.readFileSync(file);
+        fs.writeFileSync(target, fileContent);
+      }
     }
   });
 };
