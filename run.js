ca121fb713be0418a5dc7b8bd2d0f60f6419ab79
Jamb <jambdoe@gmail.com>
2021-06-10 07:02:40 -0400

    Revert "dev environment"
    This reverts commit aaf4473df6d0fb30fe5119395c5ea43b12f839d8.

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
 const path = require('path');
 const script = require(path.resolve(__dirname, 'scripts', process.argv[2]));
 
-script(path.resolve(process.cwd(), (path.dirname(__dirname).endsWith("node_modules") ? '../' : 'node_modules/') + 'typescript/bin/tsc'));
+script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
