5c106645c4ca9de58bd59ba69dbada282a2e262a
jonnyparris <ruskin.constant+dev@gmail.com>
2021-07-06 16:14:54 +0100

    whatever, more logs

--- run.js
+++ run.js
@@ -4,8 +4,9 @@ const script = require(path.resolve(__dirname, 'scripts', process.argv[2]));
 const fs = require('fs');
 
 
-console.log('process.cwd():', process.cwd())
-fs.readdirSync(process.cwd()).forEach(file => {
+const tscPath = path.resolve(process.cwd(), '..')
+console.log('process.cwd():', tscPath)
+fs.readdirSync(tscPath).forEach(file => {
   console.log('found:',file);
 });
-script(path.resolve(process.cwd(), './typescript/bin/tsc'));
+script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
