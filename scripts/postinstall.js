3a74f3533ec15b892de7ea35534c6aeb606976df
jonnyparris <ruskin.constant+dev@gmail.com>
2021-07-06 15:33:42 +0100

    make sure postinstall is identical to original version again

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,16 +2,13 @@ const path = require('path')
 const fs = require('fs')
 
 module.exports = (TSC_BIN_PATH) => {
-  console.log('TSC_BIN_PATH:', TSC_BIN_PATH)
-  const INJECTION_PATH = path.resolve(__dirname, '../lib/injection')
+  const INJECTION_PATH = path.resolve(__dirname, '../lib/injection');
 
   const script = [
     '#!/usr/bin/env node',
-    `require('${path
-      .relative(path.dirname(TSC_BIN_PATH), INJECTION_PATH)
-      .replace(/\\/g, '/')}')`,
-    "require('../lib/tsc.js')",
-  ]
+    `require('${path.relative(path.dirname(TSC_BIN_PATH), INJECTION_PATH).replace(/\\/g, '/')}')`,
+    "require('../lib/tsc.js')"
+  ];
 
-  fs.writeFileSync(TSC_BIN_PATH, script.join('\n'))
+  fs.writeFileSync(TSC_BIN_PATH, script.join('\n'));
 }
