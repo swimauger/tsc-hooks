e553684918718dfe213f640a66f58e1a4d8b5cb9
jonnyparris <ruskin.constant+dev@gmail.com>
2021-07-06 16:00:25 +0100

    log cwd when running run.js

--- run.js
+++ run.js
@@ -1,4 +1,5 @@
 const path = require('path');
 const script = require(path.resolve(__dirname, 'scripts', process.argv[2]));
 
+console.log('process.cwd():', process.cwd())
 script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
