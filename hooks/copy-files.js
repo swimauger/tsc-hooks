ce499d1c131708ee9c793b1239bff1a63c49be90
jonnyparris <ruskin.constant+dev@gmail.com>
2021-07-07 09:25:21 +0100

    revert autoformatting of copy-files hook

--- hooks/copy-files.js
+++ hooks/copy-files.js
@@ -1,19 +1,14 @@
-const glob = require('glob')
-const path = require('path')
-const fs = require('fs')
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = (tsconfig, tsconfigPath) => {
   process.on('exit', () => {
-    const tsconfigDir = path.dirname(tsconfigPath)
-    const include = tsconfig.include?.map((file) =>
-      path.resolve(tsconfigDir, file)
-    )
-    const ignore = tsconfig.exclude?.map((file) =>
-      path.resolve(tsconfigDir, file)
-    )
+    const tsconfigDir = path.dirname(tsconfigPath);
+    const include = tsconfig.include?.map(file => path.resolve(tsconfigDir, file));
+    const ignore = tsconfig.exclude?.map(file => path.resolve(tsconfigDir, file));
 
-    const files =
-      include?.reduce((files, file) => glob.sync(file, { ignore }), []) || []
+    const files = include?.reduce((files, file) => glob.sync(file, { ignore }), []) || [];
     for (const file of files) {
       if ((!tsconfig.hookOptions?.allowJS && file.endsWith('.js')) || (!tsconfig.hookOptions?.allowTS && file.endsWith('.ts'))) continue;
 
@@ -21,17 +16,17 @@ module.exports = (tsconfig, tsconfigPath) => {
       const target = path.resolve(tsconfigDir, tsconfig.compilerOptions.outDir, relative);
 
       if (!fs.existsSync(path.dirname(target))) {
-        fs.mkdirSync(path.dirname(target), { recursive: true })
+        fs.mkdirSync(path.dirname(target), { recursive: true });
       }
 
       if (fs.lstatSync(file).isDirectory() && !fs.existsSync(target)) {
-        fs.mkdirSync(target, { recursive: true })
+        fs.mkdirSync(target, { recursive: true });
       }
 
       if (fs.lstatSync(file).isFile()) {
-        const fileContent = fs.readFileSync(file)
-        fs.writeFileSync(target, fileContent)
+        const fileContent = fs.readFileSync(file);
+        fs.writeFileSync(target, fileContent);
       }
     }
-  })
-}
+  });
+};
