0f7672834208bcf5ec8d79a2088f2b09201e9af1
jonnyparris <ruskin.constant+dev@gmail.com>
2021-07-07 09:15:03 +0100

    undo run cwd logging

--- run.js
+++ run.js
@@ -1,12 +1,4 @@
 const path = require('path');
 const script = require(path.resolve(__dirname, 'scripts', process.argv[2]));
 
-const fs = require('fs');
-
-
-const tscPath = path.resolve(process.cwd(), '..')
-console.log('process.cwd():', tscPath)
-fs.readdirSync(tscPath).forEach(file => {
-  console.log('found:',file);
-});
 script(path.resolve(process.cwd(), '../typescript/bin/tsc'));
