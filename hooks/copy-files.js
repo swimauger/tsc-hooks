dc9287e6c31e3a77d95bc752e89749e256db8c36
swimauger <swimauger@gmail.com>
2021-07-31 10:08:07 -0500

    🐛 Fix config property and add arguments

--- hooks/copy-files.js
+++ hooks/copy-files.js
@@ -1,12 +1,23 @@
 module.exports = {
   dependencies: [ 'glob' ],
-  customProperties: [ 'compilerOptions.allowTS' ],
+  config: [ 'compilerOptions.allowTS' ],
+  arguments: {
+    copyFiles: [ 'cf', 'copy-files' ],
+    excludeFiles: [ 'ef', 'exclude-files' ]
+  },
   onPostCompilation: function() {
     const path = require('path');
     const fs = require('fs');
 
     const include = this.tsconfig.include?.map(file => path.resolve(this.tsconfigDir, file));
+    if (this.args.copyFiles) {
+      include.push(...this.args.copyFiles?.split(' ')?.map(file => path.resolve(process.cwd(), file.trim())));
+    }
+
     const ignore = this.tsconfig.exclude?.map(file => path.resolve(this.tsconfigDir, file));
+    if (this.args.excludeFiles) {
+      ignore.push(...this.args.excludeFiles?.split(' ')?.map(file => path.resolve(process.cwd(), file.trim())));
+    }
 
     const files = include?.reduce((files, file) => this.glob.sync(file, { ignore }), []) || [];
     for (const file of files) {
