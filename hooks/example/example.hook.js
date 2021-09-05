c7f3f7812dd7a4e06b56eb6418191afa40405bcc
swimauger <swimauger@gmail.com>
2021-09-05 15:36:26 -0500

    :sparkles: Create official version of tsc-hooks API

--- hooks/example/example.hook.js
+++ hooks/example/example.hook.js
@@ -1,21 +1,24 @@
 module.exports = {
   dependencies: [ 'chalk' ], // Needed NPM Modules, these modules will be installed using `npm i <dependency>...`
-  config: [ 'compilerOptions.message' ], // Custom properties introduced, ex: "compilerOptions.someProperty" in tsconfig
-  arguments: { // Custom arguments when using tsc
-    messageVarName: [ 'msg', 'message' ] // Used like "tsc --message 'Cool Message'" or "tsc -msg 'Cool Alias'"
+  onInitCompilation: function(api) {
+    const chalk = require('chalk');
+
+    console.log(chalk.cyan('onInitCompilation is executed before tsc has started compilation'));
+
+    console.log('Below is the api context in the onInitCompilation function:');
+    console.log(api);
+
+    console.log("Init Message:", api.tsconfig.message);
+    api.tsconfig.message = "POST MESSAGE";
   },
-  onInitCompilation: function() {
-    console.log(this.chalk.cyan('onInitCompilation is executed before tsc has started compilation'));
+  onPostCompilation: function(api) {
+    const chalk = require('chalk');
 
-    console.log('Below is the `this` context in the onInitCompilation function:');
-    console.log(this);
+    console.log(chalk.magenta('onPostCompilation is executed after tsc has completed compilation'));
 
-    console.log("Message:", this.messageVarName);
-  },
-  onPostCompilation: function() {
-    console.log(this.chalk.magenta('onPostCompilation is executed after tsc has completed compilation'));
+    console.log('Below is the api context in the onPostCompilation function:');
+    console.log(api);
 
-    console.log('Below is the `this` context in the onPostCompilation function:');
-    console.log(this);
+    console.log("Post Message:", api.tsconfig.message);
   }
 }
